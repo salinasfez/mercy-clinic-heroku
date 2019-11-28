88443fc6ad4c1aad211b26d6f23c7746120745ec
Federico Salinas <federicosalinas@federicos-air.attlocal.net>
2019-11-27 18:43:31 -0600

    Deploy to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/restaurant'
 
-mongoose.connect('mongodb://localhost:27017/restaurant', {
+mongoose.connect(MONGODB_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: true
